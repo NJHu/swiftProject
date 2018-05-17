173872f409eee9cc3da450b8706c36ffb852a477
alan <64hp@163.com>
2018-05-17 20:53:09 +0800

    navlayout

--- NJDouYu/NJDouYu/Classes/NJBase/View/NJNavigationBar.swift
+++ NJDouYu/NJDouYu/Classes/NJBase/View/NJNavigationBar.swift
@@ -11,6 +11,7 @@ import UIKit
 class NJNavigationBar: UIView {
     
     public let bottomSepLineView = UIView()
+    public let titleLabel = UILabel()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -29,7 +30,16 @@ class NJNavigationBar: UIView {
     private func setupUI() {
         backgroundColor = UIColor.white
         addSubview(bottomSepLineView)
+        addSubview(titleLabel)
         bottomSepLineView.backgroundColor = UIColor.lightGray
+        titleLabel.numberOfLines = 0
+        titleLabel.textColor = UIColor.black
+        titleLabel.translatesAutoresizingMaskIntoConstraints = false;
+        titleLabel.backgroundColor = UIColor.white
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: NSLayoutAttribute.centerX, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.width, multiplier: 1, constant: 0));
+        titleLabel.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: NSLayoutAttribute.height, relatedBy: NSLayoutRelation.equal, toItem: nil, attribute: NSLayoutAttribute.width, multiplier: 0, constant: 44))
+        titleLabel.addConstraint(NSLayoutConstraint(item: titleLabel, attribute: NSLayoutAttribute.width, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.width, multiplier: 0.7, constant: 0))
+        addConstraint(NSLayoutConstraint(item: titleLabel, attribute: NSLayoutAttribute.top, relatedBy: NSLayoutRelation.equal, toItem: self, attribute: NSLayoutAttribute.height, multiplier: 0, constant: UIApplication.shared.statusBarFrame.size.height))
     }
     
     override func layoutSubviews() {
