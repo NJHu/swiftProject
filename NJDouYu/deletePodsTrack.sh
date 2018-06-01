ce2efe35aa6b6be9c3725fb6914fec73ebab7ddb
胡鹏 <64hp@163.com>
2018-06-01 13:48:04 +0800

    add

---
+++ NJDouYu/deletePodsTrack.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git rm --cached Pods/Headers/\* --ignore-unmatch
+git rm --cached Pods/Target\ Support\ Files/\* --ignore-unmatch
+git rm --cached Pods/Local\ Podspecs/\* --ignore-unmatch
+git rm --cached Pods/Pods.xcodeproj/\* --ignore-unmatch
+git commit -m "delete pods files"
