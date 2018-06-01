c13d9a731d31714a1b0f8d035e8e00757f3069e2
胡鹏 <64hp@163.com>
2018-06-01 15:14:15 +0800

    init

--- deletePodsTrack.sh
+++ deletePodsTrack.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+git rm --cached Pods/\* --ignore-unmatch
 git rm --cached Pods/Headers/\* --ignore-unmatch
 git rm --cached Pods/Target\ Support\ Files/\* --ignore-unmatch
 git rm --cached Pods/Local\ Podspecs/\* --ignore-unmatch
 git rm --cached Pods/Pods.xcodeproj/\* --ignore-unmatch
+git rm --cached Pods/Manifest.lock/\* --ignore-unmatch
 git commit -m "delete pods files"
