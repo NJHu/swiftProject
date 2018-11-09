4eb4f114128e4060957c36f848e046f1bcfcc813
胡鹏 <64hp@163.com>
2018-11-09 20:18:24 +0800

    ??

--- SwiftProject/NJDouYu/NJAppDelegate.swift
+++ SwiftProject/NJDouYu/NJAppDelegate.swift
@@ -21,6 +21,7 @@ class NJAppDelegate: UIResponder, UIApplicationDelegate {
         window?.rootViewController = NJTabBarController()
         // 设置监控
         NJDebugTool.defaultTool.show()
+        return true
     }
     
     func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
